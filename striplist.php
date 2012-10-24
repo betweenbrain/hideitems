460a8bcbf05f4ea7c92286643b711efb790e1556
Matt Thomas <matt@betweenbrain.com>
2012-10-23 23:35:39 -0400

    Adding support for different sets of classes to remove per menu item.

--- striplist.php
+++ striplist.php
@@ -21,17 +21,19 @@ class plgSystemStriplist extends JPlugin
 			return;
 		}
 
-		$buffer  = JResponse::getBody();
+		$buffer = JResponse::getBody();
+
 		$classes = $this->params->get('classes');
-		$classes = explode(',', str_replace(' ', '', $classes));
+		$classes = explode('|', str_replace(' ', '', $classes));
 
 		$contexts = $this->params->get('contexts');
 		$contexts = explode(',', str_replace(' ', '', $contexts));
 
 		$itemId = JRequest::getInt('Itemid', 0);
 
-		foreach ($contexts as $context) {
+		foreach ($contexts as $pos => $context) {
 			if ($itemId == $context) {
+				$classes = explode(',', $classes[$pos]);
 				foreach ($classes as $class) {
 					$buffer = preg_replace('/<li( id=\"(.*?)\")? class=\"([a-zA-Z0-9-_ ]*)?\b' . $class . '\b([a-zA-Z0-9-_ ]*)?\"[^>]*>([\s\S]*?)<\/li>/i', '', $buffer);
 				}
